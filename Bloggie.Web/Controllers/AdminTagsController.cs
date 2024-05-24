f1ba9d73c36930c4bda43ca4f30070288103daed
NoOneTopVN <huynhtrongnghia15n@gmail.com>
2024-05-24 13:56:27 +0700

    Add project files.

---
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,110 @@
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly ITagRepository tagRepositories;
+
+        public AdminTagsController(ITagRepository tagRepositories)
+        {
+            this.tagRepositories = tagRepositories;
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddTagRequest addTagRequest)
+        {
+            // Mapping AddTagRequest to Tag domain model
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+            await tagRepositories.AddAsync(tag);
+
+            //return View("List");
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            //use dbContext to read the tags
+            var tags = await tagRepositories.GetAllAsync();
+
+            return View(tags);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+
+            var tag = await tagRepositories.GetAsync(id);
+
+            if (tag != null)
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName
+                };
+
+                return View(editTagRequest);
+            }
+            return View(null);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditTagRequest editTagRequest)
+        {
+            var tag = new Tag
+            {
+                Id = editTagRequest.Id,
+                Name = editTagRequest.Name,
+                DisplayName = editTagRequest.DisplayName
+            };
+
+            var updatedTag = await tagRepositories.UpdateAsync(tag);
+
+            if (updatedTag != null)
+            {
+                // Show success notofication
+            }
+            else
+            {
+                // Show error notification
+            }
+
+            //return RedirectToAction("Edit", new { id = editTagRequest.Id });
+            return RedirectToAction("List");
+        }
+
+        public async Task<IActionResult> Delete(EditTagRequest editTagRequest)
+        {
+            var deletedTag = await tagRepositories.DeleteAsync(editTagRequest.Id);
+
+            if (deletedTag != null)
+            {
+                // Show success notification
+                return RedirectToAction("List");
+            }
+
+            // Show an error notification
+            return RedirectToAction("Edit", new { id = editTagRequest.Id });
+        }
+
+    }
+}
---
+++ Bloggie.Web/Controllers/ImagesController.cs
@@ -0,0 +1,38 @@
+﻿using Bloggie.Web.Repositories;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Bloggie.Web.Controllers
+{
+    // - 200 là tốt, 400 500 là xấu
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadAsync(IFormFile file)
+        {
+            // call a repository
+            var imageURL = await imageRepository.UploadAsync(file);
+
+            if(imageURL == null)
+            {
+                return Problem("Something went wrong!", null, (int)HttpStatusCode.InternalServerError);
+            }
+
+            return new JsonResult(new { link = imageURL });
+        }
+
+    }
+}
