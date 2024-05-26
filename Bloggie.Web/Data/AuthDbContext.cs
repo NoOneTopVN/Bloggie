a470ccdf8babe7d636a7d249dabd92e96dab6625
NoOneTopVN <huynhtrongnghia15n@gmail.com>
2024-05-26 08:54:53 +0700

    8 (56)

---
+++ Bloggie.Web/Data/AuthDbContext.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bloggie.Web.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Seed Roles (User, Admin, SuperAdmin)
+
+            var adminRoleId = "990758eb-b814-41bf-bd2f-e7539bd68bf4";
+            var superAdminRoleId = "428b2ef7-9b60-415e-8e10-ed167695ed2a";
+            var userRoledId = "b7eeff6c-ae91-489e-aef9-fa538cf99600";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "Admin",
+                    Id = adminRoleId,
+                    ConcurrencyStamp = adminRoleId
+                },
+                new IdentityRole
+                {
+                    Name = "SuperAdmin",
+                    NormalizedName = "SuperAdmin",
+                    Id = superAdminRoleId,
+                    ConcurrencyStamp = superAdminRoleId
+                },
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "User",
+                    Id = userRoledId,
+                    ConcurrencyStamp = userRoledId
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+
+            // Seed SuperAdminUser
+            var superAdminId = "5fc5a141-f00e-41fd-8106-edd566b7d811";
+            var superAdminUser = new IdentityUser
+            {
+                UserName = "superadmin@bloggie.com",
+                Email = "superadmin@bloggie.com",
+                NormalizedEmail = "superadmin@bloggie.com".ToUpper(),
+                NormalizedUserName = "superadmin@bloggie.com".ToUpper(),
+                Id = superAdminId,
+            };
+
+            superAdminUser.PasswordHash = new PasswordHasher<IdentityUser>()
+                .HashPassword(superAdminUser, "Superadmin@123");
+
+            builder.Entity<IdentityUser>().HasData(superAdminUser);
+
+            // Add All roles to SuperAdminUser
+            var superAdminRoles = new List<IdentityUserRole<string>>
+            {
+                new IdentityUserRole<string>
+                {
+                    RoleId = adminRoleId,
+                    UserId = superAdminId
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = superAdminRoleId,
+                    UserId = superAdminId
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = userRoledId,
+                    UserId = superAdminId
+                },
+            };
+
+            builder.Entity<IdentityUserRole<string>>().HasData(superAdminRoles);
+        }
+    }
+}
--- Bloggie.Web/Program.cs
+++ Bloggie.Web/Program.cs
@@ -1,5 +1,6 @@
 using Bloggie.Web.Data;
 using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<BloggieDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("BloggieDbConnectionString")));
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("BloggieAuthDbConnectionString")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AuthDbContext>();
 
 builder.Services.AddScoped<ITagRepository, TagRepository>();
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepositoty>();
@@ -28,6 +35,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
